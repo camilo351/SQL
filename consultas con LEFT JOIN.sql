f932f0497d8f3ab802963d7ac68363ead05b1e9b
camilo351 <camilo.hernandez.0627@gmail.com>
2025-01-07 00:27:01 -0500

    mas ejercicos con LEFT JOIN

--- consultas con LEFT JOIN.sql
+++ consultas con LEFT JOIN.sql
@@ -101,3 +101,86 @@ LEFT JOIN pedidos
 ON clientes.cliente_id = pedidos.cliente_id
 GROUP BY clientes.nombre
 ORDER BY producto_mas_vendido DESC;
+
+
+
+CREATE TABLE clientes (
+cliente_id SERIAl PRIMARY KEY,
+nombre VARCHAR (100),
+ciudad VARCHAR (100)
+);
+
+CREATE TABLE pedidos (
+pedido_id SERIAL PRIMARY KEY,
+cliente_id INT,
+producto VARCHAR (100),
+unidades INT,
+precio NUMERIC,
+fecha_pedido DATE,
+FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_Id)
+);
+
+INSERT INTO clientes (nombre,ciudad)
+VALUES 
+('Camilo Hernandez', 'Medellín'),
+('Sofia Perez', 'Medellín'),
+('Melany Sanchez', 'Medellín'),
+('Geraldine Gonzalez', 'Barranquilla'),
+('Andrea Perez', 'Barranquilla'),
+('Jhon Salchichon', 'Bogota');
+
+INSERT INTO pedidos (cliente_id,producto,unidades,precio,fecha_pedido)
+VALUES
+(1,'Camisa',3,80000,'2024-01-05'),
+(2,'Pantalon',2,100000,'2024-02-10'),
+(3,'Zapatos',2,240000,'2024-01-15'),
+(1,'Chaqueta',1,150000,'2024-03-01'),
+(3,'Sombrero',1,20000,'2024-01-25'),
+(4,'Zapatos',1,120000,'2024-01-14');
+
+SELECT clientes.nombre, clientes.ciudad, pedidos.fecha_pedido
+FROM clientes
+LEFT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE pedidos.pedido_id IS NULL;
+
+
+
+SELECT clientes.nombre, clientes.ciudad, pedidos.producto 
+FROM clientes
+LEFT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.ciudad = 'Bogota' OR pedidos.cliente_id IS NULL
+ORDER BY clientes.ciudad ASC;
+
+
+
+SELECT clientes.nombre, pedidos.producto, COALESCE(SUM (pedidos.unidades),0) AS total_unidades
+FROM clientes
+LEFT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+GROUP BY clientes.nombre, pedidos.producto
+ORDER BY total_unidades DESC;
+
+
+
+SELECT clientes.nombre, COALESCE (pedidos.producto, 'sin pedidos') AS producto
+FROM clientes
+LEFT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE pedidos.producto = 'Zapatos' OR pedidos.cliente_id IS NULL;
+
+
+
+SELECT clientes.nombre, pedidos.producto, MAX (pedidos.precio) AS precio_maximo
+FROM clientes
+LEFT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+GROUP BY clientes.nombre, pedidos.producto
+ORDER BY precio_maximo ASC;
+
+
+
+--CONSULTAS 2 DE LEFT JOIN--
+
+
