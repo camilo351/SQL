9170adb08af34d7616a889482c7cab16a0a3846a
camilo351 <camilo.hernandez.0627@gmail.com>
2025-01-08 16:36:29 -0500

    agregando más consultas_RIGTH_JOIN

--- consultas_RIGTH_JOIN.sql
+++ consultas_RIGTH_JOIN.sql
@@ -99,3 +99,98 @@ LEFT JOIN clientes
 ON pedidos.cliente_id = clientes.cliente_id
 WHERE clientes.cliente_id IS NULL;
 
+CREATE TABLE clientes (
+cliente_id SERIAL PRIMARY KEY,
+nombre VARCHAR (100),
+ciudad VARCHAR (100),
+edad INT
+);
+
+INSERT INTO clientes (nombre,ciudad,edad) VALUES
+('Camilo Hernández', 'Barranquilla', 25),
+('Geraldine De La Asunción', 'Barranquilla', 22),
+('Juan perez', 'Cali', 27),
+('Jhon Mercado', 'Bogotá', 25),
+('Melany Sanchez', 'Medellín', 24);
+
+
+CREATE TABLE pedidos (
+pedido_id SERIAL PRIMARY KEY,
+cliente_id INT,
+producto VARCHAR (100),
+cantidad INT,
+precio_unitario INT
+);
+
+INSERT INTO pedidos (pedido_id, cliente_id, producto, cantidad, precio_unitario) VALUES
+(101, 1, 'Pantalon', 1, 100000),
+(102, 1, 'Camisa', 2, 60000),
+(103, 4, 'Zapatos', 2, 160000),
+(104, 2, 'Gorra', 3, 75000),
+(105, 7, 'Gorra', 1, 75000);
+
+SELECT clientes.nombre, pedidos.producto, pedidos.cantidad
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.edad > 23;
+
+SELECT clientes.nombre, pedidos.producto
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE pedidos.producto LIKE 'C%';
+
+SELECT clientes.nombre, pedidos.producto, pedidos.cantidad
+FROM clientes
+RIGHT JOIN pedidos 
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE pedidos.cliente_id IN (1,2,4,7)
+ORDER BY pedidos.cantidad ASC;
+
+SELECT clientes.nombre, clientes.ciudad, pedidos.producto
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.ciudad NOT IN  ('Medellin','Bogotá');
+
+SELECT COALESCE(clientes.nombre, 'Sin Cliente') AS cliente, pedidos.producto, pedidos.cantidad
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.nombre IS NULL;
+
+
+SELECT clientes.nombre, pedidos.*
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.edad BETWEEN 25 AND 30;
+
+SELECT clientes.nombre, pedidos.*
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.nombre LIKE '%a%';
+
+
+SELECT clientes.nombre, pedidos.producto
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE pedidos.producto IN ('Camisa', 'Zapatos');
+
+
+SELECT clientes.nombre, clientes.edad, pedidos.producto, pedidos.cantidad
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+WHERE clientes.edad BETWEEN 25 AND 30 AND pedidos.producto LIKE '%o%';  
+
+
+SELECT COALESCE (clientes.nombre, 'Sin cliente'),
+SUM(pedidos.cantidad * pedidos.precio_unitario) AS total_pedidos
+FROM clientes
+RIGHT JOIN pedidos
+ON clientes.cliente_id = pedidos.cliente_id
+GROUP BY clientes.nombre;
